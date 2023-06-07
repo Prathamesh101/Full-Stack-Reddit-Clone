e20d9217f93f5cd2f66df445dc29dcae5d0673cf
Prathamesh Khandade <tosheetk@gmail.com>
2023-06-07 18:04:16 -0400

    Commit Repo

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { Box } from '@mui/system';
+import React from 'react';
+import './App.css';
+import {
+  appWrapper,
+  contentWrapper,
+  sideBarWrapper,
+  contentAreaWrapper,
+  scrollListWrapper,
+  createAccountWrapper,
+  joinRedditWrapper,
+} from '@styles/styles';
+import Navbar from '@navigation/Navbar';
+import SideList from '@components/SideList';
+import { blue, grey } from '@mui/material/colors';
+import { Typography } from '@mui/material';
+import CustomButton from '@shared/CustomButton';
+import AppContentArea from '@components/AppContentArea';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Box sx={appWrapper}>
+        <Navbar />
+        <Box sx={contentWrapper}>
+          <Box sx={sideBarWrapper}>
+            <Box sx={scrollListWrapper}>
+              <SideList />
+            </Box>
+            <Box sx={createAccountWrapper}>
+              <Box sx={joinRedditWrapper}>
+                <Typography sx={{ mb: 2 }} variant="body2" gutterBottom>
+                  Create an account to follow your favorite communities and
+                  start taking part in conversations.
+                </Typography>
+                <CustomButton
+                  bgColor={blue[300]}
+                  color={grey[900]}
+                  text="Join Reddit"
+                  fullWidth
+                />
+              </Box>
+            </Box>
+          </Box>
+          <Box sx={contentAreaWrapper}>
+            <AppContentArea />
+          </Box>
+        </Box>
+      </Box>
+    </React.Fragment>
+  );
+}
+
+export default App;
